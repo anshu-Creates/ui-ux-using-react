2f86501dac9f1dd1bffa21e0ca0aedc6de0a2280
Anshu Kharwar <anshu.builds@gmail.com>
2026-05-19 18:05:56 +0530

    initial commit

---
+++ ui-ux/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Section_1 from './components/sec-1/Section-1'
+import Section_2 from './components/sec-2/Section-2'
+
+const App = () => {
+  return (
+    <div>
+      <Section_1 />
+      <Section_2 />
+    </div>
+  )
+}
+
+export default App
---
+++ ui-ux/src/components/sec-2/Section-2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Section_2 = () => {
+  return (
+    <div className='w-full h-screen bg-red-500'>Section_2</div>
+  )
+}
+
+export default Section_2
